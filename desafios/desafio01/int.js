aad6bc2ffa0ca1ea8aae4fbeef8a7227f2ed61b1
LeonardoHenrique <154704500+LeonardoHenriqueDC@users.noreply.github.com>
2024-01-13 18:25:01 -0400

    parte 1 e 2 da ativade

---
+++ desafios/desafio01/int.js
@@ -0,0 +1,22 @@
+function mudar(){
+    var msg = document.querySelector('#msg')
+    var img = document.querySelector('#img')
+    var body = document.body
+    var hora = new Date()
+    var horaatual =  6//hora.getHours()
+    msg.innerHTML = `Agora são exatamente ${horaatual} horas`
+    if ( horaatual >= 5 && horaatual <=12){
+        img.src = 'imagens/foto-manha.jpg'
+        body.style.background = '#e2cd9f'
+    }
+    else if (horaatual >=13 && horaatual <= 16){
+        img.src = 'imagens/foto-tarde.jpg'
+        body.style.background = '#b9846f'
+    }
+    else if (horaatual >= 17){
+        img.src = 'imagens/foto-noite.jpg'
+        body.style.background = 'lightblue'
+    }
+    else{}
+}
+
