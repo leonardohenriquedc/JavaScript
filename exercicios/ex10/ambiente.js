63033c2ac73331030ffa71c2b4570812b6c8210d
LeonardoHenrique <154704500+LeonardoHenriqueDC@users.noreply.github.com>
2024-01-21 12:11:22 -0400

    inicio modulo e

---
+++ exercicios/ex10/ambiente.js
@@ -0,0 +1,10 @@
+var c = 1
+while(c <6 0){
+    console.log(c)
+    c++
+}
+
+do{
+    console.log(c)
+    c++
+} while(c <= 10)
---
+++ exercicios/ex11/ex11.js
@@ -0,0 +1,3 @@
+for(var c = 0 ; c <= 10 ; c++){
+    console.log(`Ola ${c}`)
+}
