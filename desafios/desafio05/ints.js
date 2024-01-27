c2e93e7beaa8795d6350bdd6a473f09a35c41f78
LeonardoHenrique <154704500+LeonardoHenriqueDC@users.noreply.github.com>
2024-01-27 18:42:10 -0400

    Recomençando

--- desafios/desafio05/ints.js
+++ desafios/desafio05/ints.js
@@ -7,49 +7,35 @@ var divres = document.querySelector('#res2')
 var finalizar = document.querySelector('#fin')
 var elemento = []
 
-
-//-------
-function adcoption(){
-    elemento.push(Number(valor.value))
-    var option = document.createElement('option')
-    option.text= `Valor ${valor.value} adicionado`
-    select.appendChild(option)
-    
-}
-
-adicionar.addEventListener('click', adcoption)
-
-//------
-
-
-//------
-function contarop(){
-    var p = document.createElement('p')
-    p.innerHTML = `O total de valores é: ${elemento.length}`
-    res2.appendChild(p)
-
-    var cont = elemento.length
-    elemento.sort()
-    var p2 = document.createElement('p')
-    p2.innerHTML = `O maior valor é ${elemento[cont-1]}`
-    res2.appendChild(p2)
-
-    var p3 = document.createElement('p')
-    p3.innerHTML = `E o menor valor é ${elemento[0]}`
-    res2.appendChild(p3)
-
-    var p4 = document.createElement('p')
-    for(var c = cont; c >= 0; c--){
-        var valor2 = 0
-        valor2 += elemento[c]
+function Numero (n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
     }
-    p4.innerHTML = `Somando todos o valores fica: ${valor2}`
-    res2.appendChild(p4)
+    else{
+        return false
+    }
 }
 
-finalizar.addEventListener('click', contarop)
-console.log(elemento)
+function numlista(n, lista){
+    if(lista.indexOf(n) != -1){
+        return true
+    }
+    else{
+        return false
+    }
+}
 
+function adcop(){
+    if(Numero(valor.value) && numlista(valor.value, elemento)){
+        window.alert('tudo ok')
+        
+    }
+    else{
+            window.alert('ta faltando fi')
+    }
+}
+
+adicionar.addEventListener('click', adcop)
 
 
 
