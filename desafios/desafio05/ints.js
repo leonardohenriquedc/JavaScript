235f7b1fbf5d2363d30412e3e0fcce91717dbe65
LeonardoHenrique <154704500+LeonardoHenriqueDC@users.noreply.github.com>
2024-01-27 20:25:31 -0400

    atualzando

--- desafios/desafio05/ints.js
+++ desafios/desafio05/ints.js
@@ -17,7 +17,7 @@ function Numero (n){
 }
 
 function numlista(n, lista){
-    if(lista.indexOf(n) != -1){
+    if(lista.indexOf(Number(n)) != -1){
         return true
     }
     else{
@@ -26,17 +26,45 @@ function numlista(n, lista){
 }
 
 function adcop(){
-    if(Numero(valor.value) && numlista(valor.value, elemento)){
-        window.alert('tudo ok')
-        
+    if(Numero(valor.value) && !numlista(valor.value, elemento)){
+        elemento.push(Number(valor.value))
+        elemento.sort()
+        var p = document.createElement('option')
+        p.innerHTML = `Valor ${valor.value} adicionado`
+        select.appendChild(p)
+        divres.innerHTML = ''
     }
     else{
-            window.alert('ta faltando fi')
+            window.alert('Campo está em branco ou numero ja existe na lista!!!')
     }
+    valor.value = ''
+    valor.focus()
 }
 
 adicionar.addEventListener('click', adcop)
 
+function final(){
+    
+    divres.innerHTML += `<p>O total de valores na lista é ${elemento.length}</p>`
+    var maiorvalor = elemento[0]
+    var menorvalor = elemento[0]
+    var soma = 0
+    for( var pos in elemento){
+        soma += elemento[pos]
+        if(elemento[pos] > maiorvalor){
+            maiorvalor = elemento[pos]
+        }
+        if(elemento[pos] < menorvalor){
+            menorvalor = elemento[pos]
+        }
+    }
+    divres.innerHTML += `<p>O maior valor informado é: ${maiorvalor}</p>`
+    divres.innerHTML += `<p>O menor valor informado é: ${menorvalor}</p>`
+    divres.innerHTML += `<p>A soma de todos o valores é: ${soma}</p>`
+    divres.innerHTML += `<p>A media dos valores é: ${soma/elemento.length}</p>`
+}
+
+finalizar.addEventListener('click', final)
 
 
 
