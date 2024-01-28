3c0a0d29a50b07e7530bdd84d6f8c3f4d39c531a
LeonardoHenrique <154704500+LeonardoHenriqueDC@users.noreply.github.com>
2024-01-28 12:39:08 -0400

    exercitando

---
+++ exercitando-fora-parte/efp01/ins.js
@@ -0,0 +1,21 @@
+// criando as variaveis 
+
+var login = document.querySelector('#eml')
+var senha = document.querySelector('#senha')
+var entrar = document.querySelector('#entrar')
+var main = document.querySelector('#main')
+var dados = []
+
+function logado(){
+    if(login.value == '' || senha.value == ''){
+        window.alert('Algum campo está em branco por favor preencha!')
+    }
+    else{
+        dados.push(login.value)
+        dados.push(senha.value)
+        console.log(dados)
+        main.innerHTML += '<p> Você está logado!</p>'
+    }
+}
+
+entrar.addEventListener('click', logado)
